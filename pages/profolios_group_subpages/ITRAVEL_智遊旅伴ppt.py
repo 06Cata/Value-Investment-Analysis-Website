73ec163866f9004bc7dc7f629fbcfeeb0bd61c59
cata <catalinakuowork@google.com>
2024-03-26 20:36:59 +0800

    20240326

--- pages/profolios_group_subpages/ITRAVEL_智遊旅伴ppt.py
+++ pages/profolios_group_subpages/ITRAVEL_智遊旅伴ppt.py
@@ -4,7 +4,7 @@ def main():
     st.subheader("ITRAVEL 智遊旅伴")
     
     st.write('''轉職班中，和組員做過的專題。使用爬蟲、套gcp api抓資料、清洗，從台灣景點、餐廳、住宿資料提取tag，配合最後網頁使用者偏好使用，
-             之後為了不要讓行程點太遠，試著用了sklearn.cluster DBSCAN ，把景點依照距離分類。最後由前端組員，串接Flask，傳入使用者偏好的JSON檔，發出API請求，推送旅遊行程呈現在網頁
+             為了不要讓行程點太遠，用了sklearn.cluster DBSCAN ，把景點依照距離分群。最後由前端組員，串接Flask，傳入使用者偏好的JSON檔，發出API請求，推送旅遊行程呈現在網頁
 
             ''')
     st.write('')
