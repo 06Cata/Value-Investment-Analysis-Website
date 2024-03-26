734e44f0260d93c44cb665960139cb747ec23dae
cata <catalinakuowork@google.com>
2024-03-26 21:27:23 +0800

    20240326

--- pages/profolios_group_subpages/各縣市ESG公開資訊整合平臺ppt.py
+++ pages/profolios_group_subpages/各縣市ESG公開資訊整合平臺ppt.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 def main():
     st.subheader("各縣市ESG公開資訊整合平臺")
-    st.write('''轉職班中，和組員做過的專題。使用ELK平臺，爬蟲、公開資訊平台 API抓取需要資料、設定排程，Logstash整理、導入ESG相關資訊平台資料，Kibana 整理並繪製出所需畫面。最後由前端組員，使用Angular、鑲嵌iFrame、串接API至前端
+    st.write('''轉職班中，和組員做過的專題。使用ELK平臺，爬蟲、公開資訊平台 API抓取需要資料、設定排程；
+             Logstash整理、導入ESG相關資訊平台資料；Kibana 整理並繪製出所需畫面。最後由前端組員，使用Angular、鑲嵌iFrame、串接API至前端
             ''')
     st.write('')
     
