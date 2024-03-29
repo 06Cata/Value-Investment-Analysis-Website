0ff4d8aef623f7c422521ed05a4f733fda04a3e7
cata <catalinakuowork@google.com>
2024-03-29 16:52:50 +0800

    20240329

--- pages/profolios_個人_資料分析作品.py
+++ pages/profolios_個人_資料分析作品.py
@@ -5,15 +5,15 @@ def main():
     st.header("資料分析作品:")
     st.subheader(" ")
     
-    st.write("[【潛力產品、店家銷售分析 Potential products and store sales analysis】- EXCEL](https://hackmd.io/@workcata/SyERmB--T)")
+    st.write("[★ 【潛力產品、店家銷售分析 Potential products and store sales analysis】- EXCEL](https://hackmd.io/@workcata/SyERmB--T)")
     url = "https://hackmd.io/@workcata/SyERmB--T"
     st.components.v1.iframe(url, height=400) 
     st.subheader(" ")
-    st.write("[【韓文補習班新課程商業分析 Business Analysis of new Korean language school courses】- EXCEL](https://hackmd.io/@workcata/S1yMfOsfp)")
+    st.write("[★ 【韓文補習班新課程商業分析 Business Analysis of new Korean language school courses】- EXCEL](https://hackmd.io/@workcata/S1yMfOsfp)")
     url = "https://hackmd.io/@workcata/S1yMfOsfp"
     st.components.v1.iframe(url, height=400) 
     st.subheader(" ")
-    st.write("[【SF Salaries 薪水分析】ft. Kaggle - Python](https://hackmd.io/@workcata/HyAT-KhJp)")
+    st.write("[★ 【SF Salaries 薪水分析】ft. Kaggle - Python](https://hackmd.io/@workcata/HyAT-KhJp)")
     url = "https://hackmd.io/@workcata/HyAT-KhJp"
     st.components.v1.iframe(url, height=400) 
     st.subheader(" ")
