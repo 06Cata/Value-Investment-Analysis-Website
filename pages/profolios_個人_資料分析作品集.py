954e29596b281490ccb53cb5e8fc4ba1dc35131f
cata <catalinakuowork@google.com>
2024-03-31 20:02:41 +0800

    20240331

--- pages/profolios_個人_資料分析作品集.py
+++ pages/profolios_個人_資料分析作品集.py
@@ -5,6 +5,12 @@ def main():
     st.title("資料分析作品集")
     st.subheader(" ")
     
+    st.write("[★ 個人對資料分析的理解、數據相關工作經驗](https://docs.google.com/document/d/1Rc2MGMd14DQ0x_Z-9Rr5_LaAjLJ6RKy0/edit?usp=sharing&ouid=105016364475687404819&rtpof=true&sd=true)")
+    url = "https://docs.google.com/document/d/1Rc2MGMd14DQ0x_Z-9Rr5_LaAjLJ6RKy0/edit?usp=sharing&ouid=105016364475687404819&rtpof=true&sd=true"
+    st.components.v1.iframe(url, height=600, width=900) 
+    st.subheader(" ")
+    st.subheader(" ")
+    
     st.write("[★ 【潛力產品、店家銷售分析 Potential products and store sales analysis】- EXCEL](https://hackmd.io/@workcata/SyERmB--T)")
     url = "https://hackmd.io/@workcata/SyERmB--T"
     st.components.v1.iframe(url, height=400, width=700) 
@@ -23,11 +29,7 @@ def main():
     st.subheader(" ")
     st.subheader(" ")
     
-    st.write("[★ 個人對資料分析的理解、數據相關工作經驗](https://docs.google.com/document/d/1Rc2MGMd14DQ0x_Z-9Rr5_LaAjLJ6RKy0/edit?usp=sharing&ouid=105016364475687404819&rtpof=true&sd=true)")
-    url = "https://docs.google.com/document/d/1Rc2MGMd14DQ0x_Z-9Rr5_LaAjLJ6RKy0/edit?usp=sharing&ouid=105016364475687404819&rtpof=true&sd=true"
-    st.components.v1.iframe(url, height=600, width=900) 
-    st.subheader(" ")
-    st.subheader(" ")
+    
 
 if __name__ == '__main__':
     main()
